179a2bb082715511756b9737e7e15805333ab78c
TheMan180 <152086117+TheMan180@users.noreply.github.com>
2024-04-28 09:08:53 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+TOKEN = "6367969204:AAGdCJ-p0xutzm5Baeewj6jvX9vC1XAu2uk"
+IAM_TOKEN = 't1.9euelZrIx5SRmJeJi87Kz5yXz8uKy-3rnpWaz8jPnZWNlZOYnZyWj5WPjMvl8_dnC3ZP-e8dUDc3_d3z9yc6c0_57x1QNzf9zef1656VmpOaiZ6Kx43Lj4mQnJHHl52K7_zF656VmpOaiZ6Kx43Lj4mQnJHHl52KveuelZrKjImKz5yUkJuOyZyXnI6cl7XehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.EUL5-if1QppybUW9JzZz2EUri31qZn5s33alTknVOKuSpwf4BzJPp3hxYOU5VQurdIN_4rVRROUeTHNqNJe7DQ'
+FOLDER_ID = 'b1ghflfhmfe08pvkh0hu'
+
+MAX_USERS = 3
+MAX_GPT_TOKENS = 120
+COUNT_LAST_MSG = 4
+
+MAX_USER_STT_BLOCKS = 10
+MAX_USER_TTS_SYMBOLS = 5_000
+MAX_USER_GPT_TOKENS = 2_000
+
+LOGS = 'logs.txt'
+DB_FILE = 'messages.db'
+SYSTEM_PROMPT = [{'role': 'system', 'text': 'Ты веселый собеседник. Общайся с пользователем на "ты" и используй юмор. '
+                                            'Поддерживай диалог. Не объясняй пользователю, что ты умеешь и можешь. '
+                                            'Изображай человека'}]
